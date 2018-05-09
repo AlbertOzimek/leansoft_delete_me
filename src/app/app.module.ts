fe424b2f3a4b1696e113c3da654a5304124f3cc3
Ozimek <albert.ozimek@e-nov8.ch>
2018-05-09 11:33:04 +0200

    adding material checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LeansoftNavComponent } from './leansoft-nav/leansoft-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LeansoftNavComponent} from './leansoft-nav/leansoft-nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LeansoftNavComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LeansoftNavComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule, MatCheckboxModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/leansoft-nav/leansoft-nav.component.ts
+++ src/app/leansoft-nav/leansoft-nav.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
 
 @Component({
-  selector: 'leansoft-nav',
-  templateUrl: './leansoft-nav.component.html',
-  styleUrls: ['./leansoft-nav.component.css']
+    selector: 'leansoft-nav',
+    templateUrl: './leansoft-nav.component.html',
+    styleUrls: ['./leansoft-nav.component.css']
 })
 export class LeansoftNavComponent {
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+    isChecked = true;
+
+    constructor(private breakpointObserver: BreakpointObserver) {
+    }
+
+    onChange($event) {
+        console.log('checkbox clicked');
+    }
 }
